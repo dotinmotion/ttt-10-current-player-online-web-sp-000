4c4923fe4db4c2df8b7a128ed04ff14d5d80773f
Dorthy P Thielsen <dotinmotionproductions@gmail.com>
2021-05-05 19:29:01 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -52,10 +52,5 @@ def turn_count(board)
   count
 end
 
-def current_player(board)
-  if turn_count(board) % 1 == 0
-    "X"
-  else turn_count(board) % 2 == 0
-    "O"
-  end
-end
+#def current_player(board)
+ ######end
